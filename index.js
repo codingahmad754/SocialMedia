79736ca7de90af362bbba76caaf4fbbae2f509f4
codingahmad754 <123690219+codingahmad754@users.noreply.github.com>
2023-05-04 22:05:47 -0700

    add authentication and authorization

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { error } from "console";
+// import { error } from "console";
+import authRoutes from "./routes/auth.js"
+import { register } from "./controllers/auth.js"
+import { verifyToken } from "./middleware/auth.js";
 
 /*     CONFIGURATIONS    */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +37,12 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage })
+
+/* ROUTES WITH FILES */
+app.post('/auth/register', upload.single('picture'), verifyToken, register)
+
+/* ROUTES */
+app.use('/auth', authRoutes)
 
 /*  MONGOOSE SETUO   */
 const PORT = process.env.PORT || 6001;
