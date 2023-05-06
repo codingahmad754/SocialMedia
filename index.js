017e7ebb60396dd4fa7b645c80159d740937aa59
codingahmad754 <123690219+codingahmad754@users.noreply.github.com>
2023-05-05 20:59:29 -0700

    finish backend routes/controllers

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 // import { error } from "console";
-import authRoutes from "./routes/auth.js"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js"
+import { createPost } from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js";
 
 /*     CONFIGURATIONS    */
@@ -39,10 +42,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 /* ROUTES WITH FILES */
-app.post('/auth/register', upload.single('picture'), verifyToken, register)
+app.post('/auth/register', upload.single('picture'), register)
+app.post('/posts', verifyToken, upload.single("picture"), createPost)
 
 /* ROUTES */
 app.use('/auth', authRoutes)
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /*  MONGOOSE SETUO   */
 const PORT = process.env.PORT || 6001;
